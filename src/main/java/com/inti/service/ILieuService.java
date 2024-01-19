5ed01a687ab1acf45949ab3d398fa77cb9034c6d
gregmaax <greg.d.972@gmail.com>
2024-01-19 11:30:05 +0100

    ajout de la classe Lieu + repository + services + controller

--- src/main/java/com/inti/service/ILieuService.java
+++ src/main/java/com/inti/service/ILieuService.java
@@ -1,5 +1,15 @@
 package com.inti.service;
 
-public interface ILieuService {
+import com.inti.model.Lieu;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public interface ILieuService {
+    public List<Lieu> getAllLieu();
+    public Lieu saveLieu(Lieu lieu);
+    public Lieu getLieu(Long id);
+    public boolean updateLieu(Lieu lieu);
+    public void deleteLieu(Long id);
 }
